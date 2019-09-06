ba0e1d03b31ac2c8bd48c53468ef6c7a382e1117
BreensM <breensmichiel@gmail.com>
2019-09-07 00:25:31 +0200

    framework info panel

--- sketch.js
+++ sketch.js
@@ -1,9 +1,20 @@
 /// <reference path="./p5.global-mode.d.ts" />
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(800, 600);
 }
 
 function draw() {
- rect(200, 20, 20, 20);
+  drawStatsPanel()
+}
+
+function drawStatsPanel() {
+    //info panel
+    fill(189, 187, 187);
+    rect(width - 200, 0, 199, 100);
+    fill(255, 255, 255);
+    textSize(15);
+    text("Timer until dad comes home",width - 199, 20);
+    text("dollaridoos: 99999", width - 199, 40);
+    text("big flex comes  here", width - 199, 60);
 }
