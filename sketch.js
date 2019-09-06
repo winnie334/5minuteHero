df8e0b912e0ada78a64603e8d5ef439390f32bd3
BreensM <breensmichiel@gmail.com>
2019-09-07 01:09:23 +0200

    buttonlist functions

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
 /// <reference path="./p5.global-mode.d.ts" />
-
+ buttonList = [[100, 100, 200, 100, console.log]] ;
 function setup() {
   createCanvas(800, 600);
 }
 
 function draw() {
-  drawStatsPanel()
+  drawStatsPanel();
+  drawButton();
+}
+function mousePressed() {
+  for (val of buttonList) {
+   if (mouseX >= val[0] && mouseX <= val[0] + val[2] && mouseY >= val[1] && mouseY <= val[1] + val[3])
+    val[4]("yaaaaay");
+  }
+
+}
+
+function drawButton(){
+  for (val of buttonList){
+    fill (0, 0, 255);
+    rect(val[0], val[1], val[2], val[3]);
+  }
 }
 
 function drawStatsPanel() {
@@ -18,3 +33,4 @@ function drawStatsPanel() {
     text("dollaridoos: 99999", width - 199, 40);
     text("big flex comes  here", width - 199, 60);
 }
+
